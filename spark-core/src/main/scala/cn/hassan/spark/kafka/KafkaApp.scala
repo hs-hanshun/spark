b3bcab973aa884178f433100e0177a79599456e2
hanshungithub <hshanshun@163.com>
2019-10-17 19:52:53 +0800

    kafka

--- spark-core/src/main/scala/cn/hassan/spark/kafka/KafkaApp.scala
+++ spark-core/src/main/scala/cn/hassan/spark/kafka/KafkaApp.scala
@@ -5,14 +5,15 @@ import java.util.{Collections, Properties}
 import cn.hassan.spark.kafka.interceptor.ProducerPrefixInterceptor
 import org.apache.kafka.clients.consumer.KafkaConsumer
 import org.apache.kafka.clients.producer._
+import org.apache.kafka.common.TopicPartition
 import org.apache.kafka.common.serialization.StringSerializer
 
 import scala.collection.JavaConversions._
 
 object KafkaApp {
   def main(args: Array[String]): Unit = {
-    //producer()
-    consumer()
+    producer()
+    //consumer()
   }
 
   def consumer(): Unit ={
@@ -50,7 +51,7 @@ object KafkaApp {
     properties.put(ProducerConfig.INTERCEPTOR_CLASSES_CONFIG,classOf[ProducerPrefixInterceptor].getName)//添加拦截器
     val producer = new KafkaProducer[String, String](properties)
     var num = 0
-    for(i<- 1 to 5){
+    for(i<- 1 to 10){
       producer.send(new ProducerRecord(topic,"topic"+i),new Callback(){
         override def onCompletion(metadata: RecordMetadata, exception: Exception): Unit = {
           if(exception != null){
@@ -62,5 +63,40 @@ object KafkaApp {
       })
     }
     producer.close()
+  }
+
+  def offSetFun(): Unit ={
+    val props = new Properties()
+    props.put("bootstrap.servers","47.99.217.209:9092")
+    props.put("key.deserializer", "org.apache.kafka.common.serialization.StringDeserializer")
+    props.put("value.deserializer", "org.apache.kafka.common.serialization.StringDeserializer")
+    props.put("group.id", "kafka-test")
+    props.put("auto.offset.reset","earliest")
+    props.put("enable.auto.commit", "true")
+    props.put("auto.commit.interval.ms", "1000")
+
+    val consumer = new KafkaConsumer[String,String](props)
+
+    val partition = new TopicPartition("kafka-test",0)
+    consumer.assign(Collections.singleton(partition))
+
+    val lastConsumerOffset = -1L
+    while (true){
+      val records = consumer.poll(1000)
+      if (records.isEmpty){
+        return
+      }
+      val partitionRecords = records.records(partition)
+
+      partitionRecords.get(partitionRecords.size() -1).offset()
+
+      consumer.commitAsync()
+    }
+
+    println("comsumed offset id " + lastConsumerOffset)
+    val metadata = consumer.committed(partition)
+    println("commint offset is " + metadata.offset())
+    val position = consumer.position(partition)
+    println("the offset of next record is " + position)
   }
 }
